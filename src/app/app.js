41217c2425ccc7c973f7f6f9f30b664a61276174
irelion <irelion@mail.ru>
2024-10-17 08:49:22 +0300

    MADB-001: init

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import './app.css';
+
+import {Component} from "react";
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+};
